9cf4f60041ee7b173d94f24dcac8ee8f6940e4eb
calcado <pecalcado@gmail.com>
2023-03-01 14:41:29 -0300

    payment component basic css

--- src/components/Payment/index.js
+++ src/components/Payment/index.js
@@ -1,32 +1,83 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
 export default function PaymentComponent() {
+  const [clicked, setClick] = useState(false);
   return (
     <>
-      <div>Ingresso e Pagamento</div>
-      <p>Primeiro, escolha sua modalidade de Ingresso</p>
-      <TicketType>
+      <Title>Ingresso e pagamento</Title>
+      <Subtitle>Primeiro, escolha sua modalidade de Ingresso</Subtitle>
+      <Options>
         <Box>
-          <p>Presencial</p>
-          <p>R$ 250</p>
+          <Text>Presencial</Text>
+          <Price>R$ 250</Price>
         </Box>
         <Box>
-          <p>Online</p>
-          <p>R$ 100</p>
+          <Text>Online</Text>
+          <Price>R$ 100</Price>
         </Box>
-      </TicketType>
+      </Options>
+      <Subtitle>Ótimo! Agora escolha sua modalidade de hospedagem</Subtitle>
+      <Options>
+        <Box>
+          <Text>Sem Hotel</Text>
+          <Price>+ R$ 0</Price>
+        </Box>
+        <Box>
+          <Text>Com Hotel</Text>
+          <Price>+R$ 350</Price>
+        </Box>
+      </Options>
     </>
   );
 }
+
+const Title = styled.h1`
+font-size: 34px;
+font-weight: 400;
+color: #000000;
+font-family: "Roboto", sans-serif;
+margin-bottom: 37px;
+`;
+
+const Subtitle = styled.p`
+font-size: 20px;
+font-weight: 400;
+color: #8e8e8e;
+font-family: "Roboto", sans-serif;
+margin-bottom: 17px;
+`;
 
 const Box = styled.div`
 width: 145px;
 height: 145px;
 display: flex;
+margin-right: 24px;
+margin-bottom: 44px;
 flex-direction: column;
 justify-content: center;
 align-items: center;
+border-radius: 20px;
+border: 1px solid #CECECE;
+:hover{
+  cursor:pointer
+}
 `;
-const TicketType = styled.div`
+
+const Options = styled.div`
 display: flex;
+`;
+
+const Text = styled.p`
+font-size: 16px;
+font-weight: 400;
+color: #454545;
+font-family: "Roboto", sans-serif;
+margin-bottom: 3px;
+`;
+const Price = styled.p`
+font-size: 16px;
+font-weight: 400;
+color: #898989;
+font-family: "Roboto", sans-serif;
 `;
