01b093543b99d97c40694174afa32bda2b7fa88e
dcaaz <danicorreaazevedo@gmail.com>
2023-03-01 16:33:06 -0300

    feat: context

---
+++ src/pages/PaymentContext/Ayth.js
@@ -0,0 +1,14 @@
+import React, { createContext, useState } from 'react';
+
+export const AuthContext = createContext({});
+
+export default function AuthProvider({ children }) {
+  const [ticket, setTicket] = useState('');
+  const [accomodation, setAccomodation] = useState('');
+
+  return (
+    <AuthContext.Provider value={{ ticket, setTicket, accomodation, setAccomodation }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
