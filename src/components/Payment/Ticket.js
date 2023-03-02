058fc3de1b7e76503f5567515d67ab589b5a4e97
MolinaMS95 <matheusmolina95@gmail.com>
2023-03-02 17:05:38 -0300

    save

--- src/components/Payment/Ticket.js
+++ src/components/Payment/Ticket.js
@@ -1,31 +1,15 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useState } from 'react';
 import { Subtitle, Options, Box } from '../../style/paymentStyle';
 import { AuthContext } from '../../contexts/Auth.js';
 //import axios from 'axios';
 //import * as paymentApi from '../../services/paymentApi';
 import useToken from '../../hooks/useToken';
 //import api from '../../services/api';
-import { getTickets } from '../../services/paymentApi';
 
 export default function Ticket() {
   const { ticket, setTicket } = useContext(AuthContext);
   const [cards, setCards] = useState([]);
   const token = useToken();
-
-  useEffect(() => {
-    const promise = getTickets(token);
-
-    promise.then((res) => {
-      let data = res.data;
-      console.log(res);
-      setCards(data);
-    });
-
-    promise.catch((err) => {
-      console.log('erro ticket', err.response.data);
-    });
-
-  }, [setCards]);
 
   function handleClick(id) {
     setTicket(id);
@@ -44,18 +28,6 @@ export default function Ticket() {
           <h2>R$ 100</h2>
         </Box>
       </Options>
-      {/*  {tickets.map((t) =>
-        <Options key={t.id}>
-          <Box>
-            <h1>{d.modalidade}</h1>
-            <h2>{d.valor}</h2>
-          </Box>
-          <Box>
-          <h1>{d.modalidade}</h1>
-            <h2>{d.valor}</h2>
-          </Box>
-        </Options>
-      )} */}
     </>
   );
 }
