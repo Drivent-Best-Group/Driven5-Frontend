bfb4ad6ee83c911999e948837b49cb12b4484618
dcaaz <danicorreaazevedo@gmail.com>
2023-03-01 21:15:54 -0300

    feat: ticket logic

--- src/components/Payment/Ticket.js
+++ src/components/Payment/Ticket.js
@@ -1,9 +1,29 @@
-import styled from 'styled-components';
-import { useState } from 'react';
 import { Subtitle, Options, Box } from '../../style/paymentStyle';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import * as paymentApi from '../../services/paymentApi';
+import useToken from '../../hooks/useToken';
+import api from '../../services/api';
+import { getTickets } from '../../services/paymentApi';
 
 export default function Ticket() {
-  const [clicked, setClick] = useState(false);
+  const [tickets, setTickets] = useState([]);
+  const token = useToken();
+
+  useEffect(() => {
+    const promise = getTickets(token);
+
+    promise.then((res) => {
+      let data = res.data;
+      console.log(res);
+      setTickets(data);
+    });
+
+    promise.catch((err) => {
+      console.log('erro ticket', err.response.data);
+    });  
+  }, [setTickets]);
+
   return (
     <>
       <Subtitle>Primeiro, escolha sua modalidade de Ingresso</Subtitle>
@@ -17,7 +37,19 @@ export default function Ticket() {
           <h2>R$ 100</h2>
         </Box>
       </Options>
-      
+
+      {/*  {tickets.map((t) =>
+        <Options key={t.id}>
+          <Box>
+            <h1>{d.modalidade}</h1>
+            <h2>{d.valor}</h2>
+          </Box>
+          <Box>
+          <h1>{d.modalidade}</h1>
+            <h2>{d.valor}</h2>
+          </Box>
+        </Options>
+      )} */}
     </>
   );
 }
