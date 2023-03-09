28c2cf23a635b1485a38e4e3a60e9ea97a0a77de
pink_glb <adriancosta1215@gmail.com>
2023-03-09 18:01:49 -0400

    feat: getHotelRooms api created

--- src/services/hotelApi.js
+++ src/services/hotelApi.js
@@ -9,3 +9,12 @@ export async function getHotels(token) {
   
   return response.data;
 }
+
+export async function getHotelsWithRooms(token, id) {
+  const response = await api.get(`/hotels/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    },
+  });
+  return response.data;
+}
